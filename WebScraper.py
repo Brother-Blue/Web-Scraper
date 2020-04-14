2bafa9237c49992eef235d5a8cc6fd380456fbdf
Chris <gusonech@student.gu.se>
2020-04-14 18:56:31 +0200

    Removed current GUI code search method for Monster.com complete, saving method implemented

--- WebScraper.py
+++ WebScraper.py
@@ -1,56 +1,40 @@
 import requests
 import pandas as p
 import os.path
-import tkinter
 from bs4 import BeautifulSoup
 
 
-monster_title = []
-monster_company = []
-monster_location = []
-
-
-def monster_search(description, location):
+def monster_search(desc, loc):
+    monster_title = []
+    monster_company = []
+    monster_location = []
     url = "https://www.monster.com/"
-    loc = location.replace(' ', '-')
-    page = requests.get(url + description + '&where=' + loc)
+    loc = loc.replace(' ', '-')
+    page = requests.get(url + 'jobs/search/?q=' + desc + '&where=' + loc)
     content = BeautifulSoup(page.content, 'html.parser')
     res = content.find(id='SearchResults')
     elements = res.find_all('section', class_='card-content')
 
     for el in elements:
-        t = el.find('h2', class_='title')
-        c = el.find('div', class_='company')
-        l = el.find('div', class_='location')
-        if None in (t, c, l):
+        title = el.find('h2', class_='title')
+        company = el.find('div', class_='company')
+        location = el.find('div', class_='location')
+        if None in (title, company, location):
             continue
-        monster_title.append(t.text.strip())
-        monster_company.append(c.text.strip())
-        monster_location.append(l.text.strip())
-
-    return None
-
-
-def search(description):
-    # Search on all given websites using description parameter
-    return None
+        monster_title.append(title.text.strip())
+        monster_company.append(company.text.strip())
+        monster_location.append(location.text.strip())
+    return 'Monster', monster_title, monster_company, monster_location
 
 
-root = tkinter.Tk()
-root.title("Job hunter")
-tkinter.Label(root, text="Enter job description").grid(row=0)
-search_entry = tkinter.Entry(root)
-search_entry.grid(row=0, column=1)
-tkinter.Button(root, text="Exit", command=root.quit()).grid(row=5, column=0, sticky=tkinter.W, pady=5)
-tkinter.Button(root, text="Search", command=search(search_entry.get()), row=1, column=1, sticky=tkinter.W, pady=5)
+def search(description='engineer', location='New York City'):
+    return monster_search(description, location)
 
-root.mainloop()
 
-'''
-dframe = p.DataFrame({'Job Title': titles, 'Company': companies, 'Location': locations})
+def save_file(s_name="", s_title="", s_company="", s_location=""):
+    d_frame = p.DataFrame({'JOB SITE': s_name, 'JOB TITLE': s_title, 'COMPANY': s_company, 'LOCATION': s_location})
 
-save_to_path = "~/Desktop"
-file_name = input("Save file as: ")
-save_file_name = os.path.join(save_to_path, file_name + ".csv")
-dframe.to_csv(save_file_name, index=False, encoding="utf-8")
-'''
+    save_to_path = "~/Desktop"
+    file_name = input("Save file as: ")
+    save_file_name = os.path.join(save_to_path, file_name + ".csv")
+    d_frame.to_csv(save_file_name, index=False, encoding="utf-8")
