8600be8c63418be3c1a6dc9525d25d1fca5af2b2
Chris <gusonech@student.gu.se>
2020-04-14 18:15:48 +0200

    Added method that searches specifically Monster.com saving functionality unavailable until UI is set up

--- WebScraper.py
+++ WebScraper.py
@@ -1,36 +1,56 @@
 import requests
 import pandas as p
 import os.path
-
+import tkinter
 from bs4 import BeautifulSoup
 
-url = "https://www.monster.se/jobb/sok/?q=software-engineer&where=gothenburg&cy=se&intcid=swoop_HeroSearch_SE"
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+monster_title = []
+monster_company = []
+monster_location = []
 
-results = soup.find(id='SearchResults')
-job_els = results.find_all('section', class_='card-content')
 
-titles = []
-companies = []
-locations = []
+def monster_search(description, location):
+    url = "https://www.monster.com/"
+    loc = location.replace(' ', '-')
+    page = requests.get(url + description + '&where=' + loc)
+    content = BeautifulSoup(page.content, 'html.parser')
+    res = content.find(id='SearchResults')
+    elements = res.find_all('section', class_='card-content')
 
-for job in job_els:
-    title = job.find('h2', class_='title')
-    company = job.find('div', class_='company')
-    location = job.find('div', class_='location')
-    if None in (title, company, location):
-        continue
-    titles.append(title.text.strip())
-    companies.append(company.text.strip())
-    locations.append(location.text.strip())
-    print(title.text, company.text, location.text)
-    print()
+    for el in elements:
+        t = el.find('h2', class_='title')
+        c = el.find('div', class_='company')
+        l = el.find('div', class_='location')
+        if None in (t, c, l):
+            continue
+        monster_title.append(t.text.strip())
+        monster_company.append(c.text.strip())
+        monster_location.append(l.text.strip())
 
-frame = p.DataFrame({'Job Title': titles, 'Company': companies, 'Location': locations})
+    return None
+
+
+def search(description):
+    # Search on all given websites using description parameter
+    return None
+
+
+root = tkinter.Tk()
+root.title("Job hunter")
+tkinter.Label(root, text="Enter job description").grid(row=0)
+search_entry = tkinter.Entry(root)
+search_entry.grid(row=0, column=1)
+tkinter.Button(root, text="Exit", command=root.quit()).grid(row=5, column=0, sticky=tkinter.W, pady=5)
+tkinter.Button(root, text="Search", command=search(search_entry.get()), row=1, column=1, sticky=tkinter.W, pady=5)
+
+root.mainloop()
+
+'''
+dframe = p.DataFrame({'Job Title': titles, 'Company': companies, 'Location': locations})
 
 save_to_path = "~/Desktop"
 file_name = input("Save file as: ")
 save_file_name = os.path.join(save_to_path, file_name + ".csv")
-frame.to_csv(save_file_name, index=False, encoding="utf-8")
+dframe.to_csv(save_file_name, index=False, encoding="utf-8")
+'''
